289b0370e248d897281964e02954c772f07fb3f7
Daniel Fartanov <danil.fartanov@ticket.io>
2024-08-24 21:34:28 +0200

    test commit from the second account

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,36 +1,6 @@
 export const About = () => {
   return (
     <div className={'w-full mt-40 flex flex-col items-center'}>
-      {/*<div*/}
-      {/*  className={'w-140 lg:absolute lg:w-screen max-h-140 overflow-hidden'}*/}
-      {/*>*/}
-      {/*  <div className={'w-full relative'}>*/}
-      {/*    <div className={'min-w-170'}>*/}
-      {/*      <img*/}
-      {/*        src="../../public/AboutUsImages/AboutUsHeader.jpeg"*/}
-      {/*        alt="Image of our Model"*/}
-      {/*        className="w-full"*/}
-      {/*      />*/}
-      {/*    </div>*/}
-      {/*    <h1 className={'absolute bottom-10 left-16 text-xl'}>*/}
-      {/*      <span*/}
-      {/*        className={*/}
-      {/*          'bg-neutral-900 text-neutral-50 font-medium text-2xl sm:text-3xl lg:text-5xl'*/}
-      {/*        }*/}
-      {/*      >*/}
-      {/*        THE MOST*/}
-      {/*        <br />*/}
-      {/*        INSPIRING AND PERSONAL*/}
-      {/*        <br />*/}
-      {/*        FASHION ONLINE SHOP*/}
-      {/*        <br />*/}
-      {/*        IN EUROPE*/}
-      {/*      </span>*/}
-      {/*    </h1>*/}
-      {/*  </div>*/}
-      {/*</div>*/}
-      {/*<div></div>*/}
-
       <div className={'max-w-170'}>
         <div className={'w-full mt-10'}>
           <img src="/204.jpg" alt="" className={'w-full'} />
@@ -39,3 +9,4 @@ export const About = () => {
     </div>
   );
 };
+// test
